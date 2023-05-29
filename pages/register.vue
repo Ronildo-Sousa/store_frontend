875ea5e3fda15eedbfbd13c8d20df0565e8f5718
Ronildo Sousa <sousaronildo400@gmail.com>
2023-05-29 13:21:47 -0300

    feat: show the validation errors on the register form

--- pages/register.vue
+++ pages/register.vue
@@ -1,3 +1,42 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const http = axios.create({
+  baseURL: "http://localhost/api",
+});
+
+const errors = ref([]);
+
+const getErrors = (field) => {
+  let result = null;
+  errors.value.forEach((error) => {
+    if (Object.keys(error)[0] === field) {
+      result = error;
+    }
+  });
+  return result !== null ? result[field] : null;
+};
+
+const login = (event) => {
+  event.preventDefault();
+  errors.value = [];
+
+  http
+    .post("/auth/register")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      let validationErrrors = Object.entries(error.response.data.errors);
+
+      validationErrrors.forEach((item) =>
+        item.reduce((key, value) => errors.value.push({ [key]: value }))
+      );
+    });
+};
+</script>
+
 <template>
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -12,9 +51,8 @@
         Sign up to your account
       </h2>
     </div>
-
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" method="POST">
         <div class="flex justify-between gap-2">
           <div class="w-full">
             <label
@@ -27,9 +65,13 @@
                 id="first_name"
                 name="first_name"
                 type="text"
-                required
                 class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
               />
+            </div>
+            <div v-if="getErrors('first_name')" class="text-red-500">
+              <small v-for="(error, index) in getErrors('first_name')" :key="index">
+                {{ error }}
+              </small>
             </div>
           </div>
 
@@ -44,9 +86,13 @@
                 id="last_name"
                 name="last_name"
                 type="text"
-                required
                 class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
               />
+            </div>
+            <div v-if="getErrors('last_name')" class="text-red-500">
+              <small v-for="(error, index) in getErrors('last_name')" :key="index">
+                {{ error }}
+              </small>
             </div>
           </div>
         </div>
@@ -60,9 +106,14 @@
               name="email"
               type="email"
               autocomplete="email"
-              required
               class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
             />
+          </div>
+
+          <div v-if="getErrors('email')" class="text-red-500">
+            <small v-for="(error, index) in getErrors('email')" :key="index">
+              {{ error }}
+            </small>
           </div>
         </div>
 
@@ -80,9 +131,14 @@
               name="password"
               type="password"
               autocomplete="current-password"
-              required
               class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
             />
+          </div>
+
+          <div v-if="getErrors('password')" class="text-red-500">
+            <small v-for="(error, index) in getErrors('password')" :key="index">
+              {{ error }}
+            </small>
           </div>
         </div>
 
@@ -99,14 +155,23 @@
               id="password_confirmation"
               name="password_confirmation"
               type="password"
-              required
               class="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
             />
+          </div>
+
+          <div v-if="getErrors('password_confirmation')" class="text-red-500">
+            <small
+              v-for="(error, index) in getErrors('password_confirmation')"
+              :key="index"
+            >
+              {{ error }}
+            </small>
           </div>
         </div>
 
         <div>
           <button
+            @click="login"
             type="submit"
             class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >

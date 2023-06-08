fd19a0b914e7cccd9eca848f4164acbe57a41624
Ronildo Sousa <sousaronildo400@gmail.com>
2023-06-08 11:11:40 -0300

    feat: wip product list and card

---
+++ components/product/List.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(["products"]);
+</script>
+
+<template>
+  <div class="mt-5 mb-5">
+    <h3 class="ml-3 text-lg font-semibold">Algum titulo here....</h3>
+    <div class="inline-flex gap-5">
+      <ProductCard
+        v-for="product in products.items"
+        :key="product.slug"
+        :product="product"
+      />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { onMounted, ref } from "vue";
+const { $axios } = useNuxtApp();
+
+const products = ref({ items: null });
+
+onMounted(() => {
+    $axios().get('/products/?from_categories[]=odell-stark-iv').then((response) => {
+      products.value.items = response.data.data;
+    })
+});
+</script>
+
 <template>
-  <NuxtLayout> </NuxtLayout>
+  <NuxtLayout>
+    <div class="p-5 dark:bg-gray-700 dark:text-white">
+      <ProductList :products="products" />
+    </div>
+  </NuxtLayout>
 </template>

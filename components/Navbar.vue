39987e336d24a6db5b9ffd156097e747dfcf95a3
Ronildo Sousa <sousaronildo400@gmail.com>
2023-06-09 13:06:02 -0300

    feat: creates a default modal component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -82,10 +82,64 @@
             ></path>
           </svg>
         </button>
+        <div>
+          <!-- toggle cart -->
+          <button
+            type="button"
+            data-modal-target="defaultModal"
+            data-modal-toggle="defaultModal"
+            class="relative p-2 ml-2 text-xs text-gray-500 rounded dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+            <span
+              class="absolute right-0 flex items-center justify-center w-1 h-1 p-2 text-xs font-semibold text-white bg-red-500 rounded-full top-1/2"
+              >1</span
+            >
+          </button>
+        </div>
         <div id="dark-toggle" class="flex ml-2 item-center">
-          <button id="theme-toggle" type="button" class="p-2 text-xs text-gray-500 rounded dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
-              <svg id="theme-toggle-dark-icon" class="hidden w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-              <svg id="theme-toggle-light-icon" class="hidden w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+          <button
+            id="theme-toggle"
+            type="button"
+            class="p-2 text-xs text-gray-500 rounded dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700"
+          >
+            <svg
+              id="theme-toggle-dark-icon"
+              class="hidden w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+              ></path>
+            </svg>
+            <svg
+              id="theme-toggle-light-icon"
+              class="hidden w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </button>
         </div>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,71 @@
 <script setup>
-import { onMounted, onBeforeMount, ref } from "vue";
+import { onMounted, ref } from "vue";
 const { $axios } = useNuxtApp();
 
 const categories = ref({ items: null });
 const products = ref([]);
 
-onMounted(() => {
-  $axios()
-    .get("/categories")
-    .then((response) => {
-      categories.value.items = response.data.data;
-      categories.value.items.forEach((category) => {
-        $axios()
-          .get(`/products/?from_categories[]=${category.slug}`)
-          .then((response) => {
-            products.value.push({ category: category.name, items: response.data.data });
-          });
-      });
-    });
-});
+// onMounted(() => {
+//   $axios()
+//     .get("/categories")
+//     .then((response) => {
+//       categories.value.items = response.data.data;
+//       categories.value.items.forEach((category) => {
+//         $axios()
+//           .get(`/products/?from_categories[]=${category.slug}`)
+//           .then((response) => {
+//             products.value.push({ category: category.name, items: response.data.data });
+//           });
+//       });
+//     });
+// });
 </script>
 
 <template>
   <NuxtLayout>
     <div class="p-5 dark:bg-gray-700 dark:text-white">
-      <ProductList
+      <SharedDefaultModal>
+        <template #header>
+          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            Terms of Service
+          </h3>
+        </template>
+        <template #body>
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            With less than a month to go before the European Union enacts new consumer
+            privacy laws for its citizens, companies around the world are updating their
+            terms of service agreements to comply.
+          </p>
+          <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            The European Unions General Data Protection Regulation (G.D.P.R.) goes into
+            effect on May 25 and is meant to ensure a common set of data rights in the
+            European Union. It requires organizations to notify users as soon as possible
+            of high-risk data breaches that could personally affect them.
+          </p>
+        </template>
+        <template #footer>
+          <button
+            data-modal-hide="defaultModal"
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            I accept
+          </button>
+          <button
+            data-modal-hide="defaultModal"
+            type="button"
+            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+          >
+            Decline
+          </button>
+        </template>
+      </SharedDefaultModal>
+      <!-- <ProductList
         v-for="item in products"
         :key="item.category"
         :products="item"
         :list-title="item.category"
-      />
+      /> -->
     </div>
   </NuxtLayout>
 </template>
